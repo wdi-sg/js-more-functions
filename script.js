ee4ed4b2476565441bc3db71f814e6eab8940995
abdulhakimsg <abdulhakimchi@gmail.com>
2018-09-11 10:36:25 +0800

    finished till part 3

--- script.js
+++ script.js
@@ -3,35 +3,49 @@
 // Exercise 1: Fizzbuzz
 // ====================
 // Write a function that loops through numbers from 1 to 100 and does the following:
-//   If the number is divisible by 3, print Fizz.
-//   If the number is divisible by 5, print Buzz.
-//   If the number is divisible by both 3 and 5, print FizzBuzz.
-//   If the number is divisible by neither 3 nor 5, print the number.
 // Hint: The % operator gives the remainder when one number is divided by another, e.g. 10 % 7 gives 3.
 // Note: This used to be a common interview question.
 
-
-
-
-
-
-
-
-
+  // If the number is divisible by 3, print Fizz.
+  // If the number is divisible by 5, print Buzz.
+for(var i = 1 ; i<=100 ; i++){
+    if(i%3 == '0'){
+    console.log('Fizz' , i)
+    }
+    else if (i%5 == '0')
+    console.log('Buzz' , i)
+}
+  // If the number is divisible by both 3 and 5, print FizzBuzz.
+  // If the number is divisible by neither 3 nor 5, print the number.
+for(var i = 1 ; i<=100 ; i++){
+    if((i%3 == '0') && (i%5 == '0')){
+    console.log('FizzBuzz');
+    }
+    else{
+    console.log(i)
+    }
+}
 
 // Exercise 2: Prime Counting
 // ==========================
 // Write a function that takes a number and returns true if the number is prime, and false otherwise.
 // Hint: A number is prime when it is only divisble by 1 and itself.
 
+function isPrime(num) {
+    for(var i=2; i<num; i++) {
+        if(num % i == 0) {
+            prime = false;
+            console.log(num + ' is not a prime number')
+            break;
+        }
+        else{
+            console.log(num + ' is a prime number')
+        }
+    }
+    return num;
+}
 
-
-
-
-
-
-
-
+isPrime(7)
 
 // Exercise 3: Letter Counting
 // ===========================
@@ -42,11 +56,32 @@
 // e - 1
 // Hint: Somewhere, you will need to examine each letter in the string, and increase the value of a counter depending on the letter you're currently examining. An object with keys corresponding to the letters of the string would be useful.
 
+//create string
+var input = "KkanyeWest"
+var str = input.toLowerCase()
 
+//create object to put in character
+var counts = {}
 
+//initialise character
+var ch
+//initialise count
+var count
 
+//create a loop to run through length of string
+for(var i = 0 ; i <str.length ; i++){
+    //start with first character of string
+    ch = str.charAt(i)
+    //if character appear once insert in counts ; define as count
+    count = counts[ch]
+    // if character count appear again in counts ; user ternary + 1 or just assign number 1
+    counts[ch] = count ? count + 1 : 1
+}
 
-
+for (ch in counts) {
+    //print character , add str count : , with value of count[ch]
+    console.log(ch + " count is " + counts[ch]);
+}
 
 
 
@@ -78,11 +113,6 @@ console.log("The value of x is", x, "and the value of y is", y);
 // 5A. Write a function that takes 2 arrays of numbers and returns an array containing only the unique elements in both arrays. For example, union([1, 2, 3], [2, 3, 4]) should return [1, 2, 3, 4].
 // Hint: The .indexOf method of an array can find you the index of a given element in an array.
 // *Optional challenge: add an optional 3rd parameter, to be a boolean value, so that when it is true, the array that you return is sorted.
-
-
-
-
-
 
 
 
